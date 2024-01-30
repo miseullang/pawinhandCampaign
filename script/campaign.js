9020c32cfff97955f2fc0809cd8768a88a01dd9f
miseullang <155376060+miseullang@users.noreply.github.com>
2024-01-30 15:43:25 +0900

    Add files via upload

--- script/campaign.js
+++ script/campaign.js
@@ -1,3 +1,4 @@
+// 숨김메뉴 토글
 const menuButs = document.querySelectorAll(".menuBut");
 const hiddenMenuBg = document.querySelector(".hiddenMenuBg")
 menuButs.forEach(menuBut => {
@@ -5,3 +6,12 @@ menuButs.forEach(menuBut => {
         hiddenMenuBg.style.display = (hiddenMenuBg.style.display === "none") ? "block" : "none";
     })
 });
+
+// 검색창 열기
+
+// 검색창 닫기
+const searchModal = document.querySelector('.searchModal');
+
+function searchToggle() {
+    searchModal.style.display = (searchModal.style.display === "none") ? "block" : "none";
+}
